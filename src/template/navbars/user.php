96f9dea8961570b502cbdbd001f87ffae362f4c7
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2026-01-14 17:24:40 +0100

    feat: add logged user navbar

---
+++ src/template/navbars/user.php
@@ -0,0 +1,10 @@
+                    <li class="nav-item">
+                        <a class="nav-link" href="manage_reservations.php">
+                            Prenotazioni
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="reviews.php">
+                            Recensioni
+                        </a>
+                    </li>
