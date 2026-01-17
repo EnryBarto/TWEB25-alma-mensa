14f3d3d720d08045140318bf28aa651c1f3e92cf
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2026-01-17 13:14:46 +0100

    fix: show modify only if you manage the canteen

--- src/template/canteen_details.php
+++ src/template/canteen_details.php
@@ -43,11 +43,13 @@
                     <a class="btn btn-primary" role="button" href="reservation.php"><span class="bi bi-journal-check"></span>&nbsp;Prenota</a>
                 </div>
             <?php break;
-                case UserLevel::CanteenAdmin: ?>
+                case UserLevel::CanteenAdmin: 
+                    if($user->getId() === $canteen->getId()):?>
                     <div class="col-10 col-md-4 offset-md-1 d-grid gap-2">
                         <a href="manage_canteen.php" role="button" class="btn btn-primary"><span class="bi bi-pen-fill"></span> Modifica profilo</a>
                         <a href="change_password.php" role="button" class="btn btn-primary"><span class="bi bi-key-fill"></span> Modifica password</a>
                     </div>
                 <?php break; ?>
-            <?php endswitch; ?>
+            <?php endif;
+            endswitch; ?>
         </section>
