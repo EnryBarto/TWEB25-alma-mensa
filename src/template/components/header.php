3f6e800fd939813b5455222d707002e6499a1123
nico <nicholasbenedetti873@gmail.com>
2026-01-21 23:18:56 +0100

    fix: made page accessible

--- src/template/components/header.php
+++ src/template/components/header.php
@@ -1,4 +1,4 @@
 <header class="mt-4 text-center container">
-    <h1><?php echo $templateParams["h1"]; ?></h1>
+    <h3><?php echo $templateParams["h1"]; ?></h3>
     <p class="text-body-secondary"><?php echo $templateParams["subtitle"]; ?></p>
 </header>
